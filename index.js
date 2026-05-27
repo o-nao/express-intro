8ddfea486836a675116b394c5d37278d954a4190
o-nao <nao0117.08194@icloud.com>
2026-05-27 11:47:54 +0000

    index.jsの変更

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+const loggerMiddleware = function(req, res, next) {
+  console.log(`[${new Date()}] ${req.method} ${req.url}`);
+  next();
+};
+
+app.use(loggerMiddleware);
+
+// 静的ファイルの提供とキャッシュの有効化
+app.use(express.static('public', { maxAge: 86400000 }));
+
+// test-imageリクエスト
+app.get('/test-image', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/images/test.png'));
+});
 
 // GETリクエスト
 app.get('/', (req, res) => {
